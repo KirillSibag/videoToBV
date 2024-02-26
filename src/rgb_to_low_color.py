e9fb485f4021c6af910dd3b7044879dd222c1039
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2024-02-26 23:30:22 +0300

    fix: The program is optimized

--- src/rgb_to_low_color.py
+++ src/rgb_to_low_color.py
@@ -1,32 +1,46 @@
-from PIL import Image, ImageFilter
+import os
+
 import cv2
 
-vidcap = cv2.VideoCapture('vid.mp4')
-success, image = vidcap.read()
-count = 0
-height, width, layers = image.shape
-size = (width,height)
-out = cv2.VideoWriter('cartoon.avi',cv2.VideoWriter_fourcc(*'DIVX'), 3, size)
 
-h = 2
-n = 25
-new = 7
-while success:
-    rows,cols,_ = image.shape
-    img1 = Image.new("RGB", (cols, rows), 'black')
-            
-    for i in range(rows):
-        for j in range(cols):
-            r = image[i,j][2] // new*new
-            g = image[i,j][1] // new*new
-            b = image[i,j][0] // new*new
+PROJECT_DIR = os.getenv("PROJECT_DIR")
 
-            img1.putpixel((j,i), (r // n * n + (n//h), g // n * n + (n//h), b // n * n + (n//h)))
 
-##    img1 = img1.filter(ImageFilter.FIND_EDGES)
-    img1 = img1.convert("L")
-    img1.save(str(count) + ".jpg")
-        
-    count += 1
+def main() -> int:
+    cap = cv2.VideoCapture(f"{PROJECT_DIR}/res/vid.mp4")
+    if not cap.isOpened():
+        print("Cannot open input video")
+        cap.release()
+        return -1
 
-    success, image = vidcap.read()
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+    size = cap.read()[1].shape[:2]
+    out = cv2.VideoWriter(f"{PROJECT_DIR}/res/out.mp4", fourcc, 30, size)
+    if not out.isOpened():
+        print("Cannot open output video")
+        cap.release()
+        out.release()
+        return -1
+
+    code = 0
+    while cv2.waitKey(1) != ord("q"):
+        success, frame = cap.read()
+        if not success:
+            print("Can't receive frame (stream end?). Exiting ...")
+            code = -1
+            break
+
+        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        out.write(frame)
+
+        cv2.imshow("RGB", frame)
+        cv2.imshow("GRAY", frame_gray)
+
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+    return code
+
+
+if __name__ == "__main__":
+    exit(main())
